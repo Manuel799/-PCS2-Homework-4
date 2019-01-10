1442d862329bb5f523e4b1bfbcb438d102c947be
NinhoMCJ <45490962+NinhoMCJ@users.noreply.github.com>
2019-01-10 21:25:25 +0100

    Delete RNA.py

--- RNA.py
+++
@@ -1,6 +0,0 @@
-with open('rosalind_rna.txt') as Data_RNA:
-	dna = Data_RNA.read().strip()
-
-with open('rosalind_rna.txt', 'w') as RNA:
-	RNA.write(dna.replace('T', 'U'))
-	print (dna.replace('T', 'U'))
