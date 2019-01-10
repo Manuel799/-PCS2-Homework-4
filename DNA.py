3e2850d07ab9803bb26946c5d0a07f398207e0df
NinhoMCJ <45490962+NinhoMCJ@users.noreply.github.com>
2019-01-10 21:24:57 +0100

    Delete DNA.py

--- DNA.py
+++
@@ -1,9 +0,0 @@
-with open('rosalind_dna.txt') as Data_DNA:
-	dna = Data_DNA.read()
-
-c_nucleotides = []
-for nuc in ['A', 'C', 'G', 'T']:
-	c_nucleotides.append(str(dna.count(nuc)))
-
-print (' '.join(c_nucleotides))
-
